ad1df413a65086772ba98beb54e728721161d496
francois <ninanou>
2017-01-12 09:48:37 +0000

    test clavier avec R220 en direct sur GPIO en sortie de Edison - schemaClavier1 -  OK

--- clavier4.py
+++ clavier4.py
@@ -1,29 +1,21 @@
 import time
 import mraa
 
-#Les niveaux logiques des GPIO ne sont pas tous les memes (1.8 ou 3.3) 
+# ESSAIS AVEC LA BREAKOUT BOARD en direct sur les pins avec 4 resistances de 220 à la masse sous les INputs
+ 
+mraa.Gpio(35).dir(mraa.DIR_OUT) # J19-8
+mraa.Gpio(26).dir(mraa.DIR_OUT)	# J18-13	
+mraa.Gpio(25).dir(mraa.DIR_OUT)	# J18-12
+mraa.Gpio(13).dir(mraa.DIR_OUT)	# J17-14
+mraa.Gpio(21).dir(mraa.DIR_IN)	# J18-8
+mraa.Gpio(0).dir(mraa.DIR_IN)	# J17-1
+mraa.Gpio(20).dir(mraa.DIR_IN)	# J18-7
+mraa.Gpio(14).dir(mraa.DIR_IN)	# J18-1
+				# J18-3 : GND
 
-
-#CLAVIER = [['1', '2', '3', 'A'], ['4', '5', '6', 'B'], ['7', '8', '9','C'], ['*', '0', '#', 'D']]
-#LIGNE   = [35, 26, 25, 13]   # GP : 131, 130, 129, 128  
-#COLONNE = [21, 0, 20, 14]    # GP : 183, 182, 12, 13
-
-#for j in range (4) : 
-#	mraa.Gpio(COLONNE[j]).dir(mraa.DIR_OUT)
-#	mraa.Gpio(LIGNE[j]).dir(mraa.DIR_IN)
-
-mraa.Gpio(35).dir(mraa.DIR_OUT)
-mraa.Gpio(26).dir(mraa.DIR_OUT)
-mraa.Gpio(25).dir(mraa.DIR_OUT)
-mraa.Gpio(13).dir(mraa.DIR_OUT)
-mraa.Gpio(21).dir(mraa.DIR_IN)
-mraa.Gpio(0).dir(mraa.DIR_IN)
-mraa.Gpio(20).dir(mraa.DIR_IN)
-mraa.Gpio(14).dir(mraa.DIR_IN)
-
-mraa.Gpio(35).write(0)
+mraa.Gpio(35).write(1)
 mraa.Gpio(26).write(0)
-mraa.Gpio(25).write(1)	#Colonne 3 active
+mraa.Gpio(25).write(0)	#Colonne 3 active
 mraa.Gpio(13).write(0)       
 
 while True :	
@@ -39,3 +31,4 @@ while True :
 	if mraa.Gpio(14).read() == 1: 
                 print 'L4'
                 time.sleep(0.2)
+
